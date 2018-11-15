8a1418aebf795c2cf84a0f21eaea7ad7b6a758b7
Matt Christie <christiemj09@gmail.com>
2018-11-15 11:52:41 -0600

    Added config.get_kwargs().

--- config.py
+++ config.py
@@ -6,11 +6,16 @@ import json
 
 
 def from_config(func):
-	"""Run a function from a JSON configuration file."""
-	
-	def decorator(filename):
-		with open(filename, 'r') as file_in:
-			config = json.load(file_in)
-		return func(**config)
-	
-	return decorator
+    """Run a function from a JSON configuration file."""
+    
+    def decorator(filename):
+        with open(filename, 'r') as file_in:
+            config = json.load(file_in)
+	    return func(**config)
+    
+    return decorator
+
+
+def get_kwargs(**kwargs):
+    """Get kwargs."""
+    return kwargs
